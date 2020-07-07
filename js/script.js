ac48ef6b4047a6e014d39a864b1d49324267d78a
fabrynet <fabrynet.bianco@gmail.com>
2020-07-07 16:31:39 +0200

    Sezione Jumbo.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // comprsa menu dropdown al passaggio del mouse
 $(document).ready(function(){
-  $(".parent-dropdown").mouseenter(function() {
+  $(".parent-dropdown").mouseenter(function(){
     $(this).children('.dropdown').toggle();
   });
-  $(".parent-dropdown").mouseleave(function() {
+  $(".parent-dropdown").mouseleave(function(){
     $(this).children('.dropdown').toggle();
   });
 });
