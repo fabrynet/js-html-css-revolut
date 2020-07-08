7587729baef887756c94785c6448348e9ad99aca
fabrynet <fabrynet.bianco@gmail.com>
2020-07-08 13:17:52 +0200

    Sistemazione e semplificazione jquery

--- js/script.js
+++ js/script.js
@@ -1,21 +1,45 @@
 $(document).ready(function(){
-  console.log($(window).width());
-  var viewport = $(window).width();
-  // comparsa menu dropdown al passaggio del mouse con viewport > 1023px
-  if (viewport > 1023) {
-    $(".parent-dropdown").mouseenter(function(){
-      $(this).children('.dropdown').toggle();
+  // comparsa dropdown menu al passaggio del mouse
+  $(".parent-dropdown").mouseenter(function(){
+    $(this).children('.dropdown').toggle();
+  });
+  $(".parent-dropdown").mouseleave(function(){
+    $(this).children('.dropdown').toggle();
+  });
+  // comparsa dell'hamburger menu al click sull'icona
+  $(".button-hamburger-menu").click(function(){
+    $('.header-hamburger-menu').fadeIn('slow',function(){
+      $('.header-hamburger-menu').addClass('active')
     });
-    $(".parent-dropdown").mouseleave(function(){
-      $(this).children('.dropdown').toggle();
+  });
+  $(".close-hamburger-menu").click(function(){
+    $('.header-hamburger-menu').fadeOut('slow',function(){
+      $('.header-hamburger-menu').removeClass('active')
     });
-    // comparsa menu hamburger al click del mouse con viewport < 1023px
-  } else {
-    $(".button-hamburger-menu").click(function(){
-      $('.header-hamburger-menu').addClass('active');
-    });
-    $(".close-hamburger-menu").click(function(){
-      $('.header-hamburger-menu').removeClass('active');
-    });
-  }
+  });
+  // console.log($(window).width());
+  // $(window).resize(function(){
+  //   var viewport = $(window).width();
+  //   // comparsa menu dropdown al passaggio del mouse con viewport > 1023px
+  //   if (viewport > 1023) {
+  //     $(".parent-dropdown").mouseenter(function(){
+  //       $(this).children('.dropdown').toggle();
+  //     });
+  //     $(".parent-dropdown").mouseleave(function(){
+  //       $(this).children('.dropdown').toggle();
+  //     });
+  //     // comparsa menu hamburger al click del mouse con viewport < 1023px
+  //   } else {
+  //     $(".button-hamburger-menu").click(function(){
+  //       $('.header-hamburger-menu').fadeIn('slow',function(){
+  //         $('.header-hamburger-menu').addClass('active')
+  //       });
+  //     });
+  //     $(".close-hamburger-menu").click(function(){
+  //       $('.header-hamburger-menu').fadeOut('slow',function(){
+  //         $('.header-hamburger-menu').removeClass('active')
+  //       });
+  //     });
+  //   }
+  // });
 });
