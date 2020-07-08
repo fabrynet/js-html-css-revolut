00468222c4ae830112dab2a6d5e399965f15d8a2
fabrynet <fabrynet.bianco@gmail.com>
2020-07-08 11:05:49 +0200

    Impostazione hamburger menu.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// comprsa menu dropdown al passaggio del mouse
 $(document).ready(function(){
-  $(".parent-dropdown").mouseenter(function(){
-    $(this).children('.dropdown').toggle();
-  });
-  $(".parent-dropdown").mouseleave(function(){
-    $(this).children('.dropdown').toggle();
-  });
+  console.log($(window).width());
+  var viewport = $(window).width();
+  // comparsa menu dropdown al passaggio del mouse con viewport > 1023px
+  if (viewport > 1023) {
+    $(".parent-dropdown").mouseenter(function(){
+      $(this).children('.dropdown').toggle();
+    });
+    $(".parent-dropdown").mouseleave(function(){
+      $(this).children('.dropdown').toggle();
+    });
+    // comparsa menu hamburger al click del mouse con viewport < 1023px
+  } else {
+    $(".button-hamburger-menu").click(function(){
+      $('.header-hamburger-menu').addClass('active');
+    });
+    $(".close-hamburger-menu").click(function(){
+      $('.header-hamburger-menu').removeClass('active');
+    });
+  }
 });
